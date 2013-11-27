f0fe083042e604ca811428c5a878306c6b1a63b0
jamie scott <webmaster@modderscentral.com>
2013-11-27 16:04:23 +0000

    uploaded

---
+++ litecoingen/litecoingen/Form1.vb
@@ -0,0 +1,55 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+Imports System.Text.RegularExpressions
+'//add your own error checks 
+'//im not your personal coder. or am i?
+Public Class Form1
+    Public Function litecoin(ByVal URL As String, ByVal contnt As String)
+        Dim gdf As Byte() = Encoding.UTF8.GetBytes(contnt)
+        Dim Req As HttpWebRequest
+        Dim SourceStream As System.IO.Stream
+        Dim Response As HttpWebResponse
+
+        Try
+
+            'create a web request to the URL  
+            Req = HttpWebRequest.Create("http://192.168.0.11:9432")
+            Req.Credentials = New NetworkCredential("litecoinerzunite", "youwillneverguessthispass")
+
+            Req.ContentType = "application/json-rpc"
+            Req.Method = "POST"
+            Dim dataStream As Stream = Req.GetRequestStream()
+            dataStream.Write(gdf, 0, gdf.Length)
+            dataStream.Close()
+            Dim webResponlse As WebResponse = Req.GetResponse()
+            Dim loResponseStream As StreamReader = New StreamReader(webResponlse.GetResponseStream())
+            Return loResponseStream.ReadToEnd()
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Return "0"
+
+        End Try
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '''''''''''''''''''''''''
+        MsgBox(getpair)
+    End Sub
+    Function getpair()
+        Dim address As String = String.Empty
+        Dim privkey As String = String.Empty
+        Dim getaddress As String = litecoin("", "{""method"":""getnewaddress"",""params"":[""""],""id"":1}")
+        Dim theaddress As Match = Regex.Match(getaddress, "{""result"":""([A-Za-z0-9\-]+)"",""error"":null,""id"":1}")
+        If (theaddress.Success) Then
+            address = (theaddress.Groups(1).Value)
+        End If
+        '''''''''''''''''''''''''
+        Dim privatekey As String = litecoin("", "{""method"":""dumpprivkey"",""params"":[""" & address & """],""id"":1}")
+        Dim theakey As Match = Regex.Match(privatekey, "{""result"":""([A-Za-z0-9\-]+)"",""error"":null,""id"":1}")
+        If (theakey.Success) Then
+            privkey = (theakey.Groups(1).Value)
+        End If
+        Return "address: " & address & vbNewLine & "privkey: " & privkey
+    End Function
+End Class
---
+++ litecoingen/litecoingen/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("litecoingen")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("litecoingen")> 
+<Assembly: AssemblyCopyright("Copyright ©  2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("990bb533-7bdb-4874-b14d-e5a2bf3d616a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
